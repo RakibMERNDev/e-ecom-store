dcdfea5da26d291ae4a65282962ee020f04fe62e
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-28 04:36:53 +0600

    endpoint updated for refresh token

--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,12 +1,17 @@
 import { Router } from "express";
 
-import { login, logout, signup, refreshToken } from "../controllers/auth.controller.js";
+import {
+  login,
+  logout,
+  signup,
+  refreshToken,
+} from "../controllers/auth.controller.js";
 
 const router = Router();
 
 router.post("/signup", signup);
 router.post("/login", login);
 router.post("/logout", logout);
-router.post("/refresh", refreshToken);
+router.post("/refresh-token", refreshToken);
 
 export default router;
