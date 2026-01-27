a8f5e3f543ac7d5303920ce81d9f489290cbbb32
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-27 21:07:11 +0600

    initial server setup completed

---
+++ backend/controllers/auth.controller.js
@@ -0,0 +1,11 @@
+export const signup = async (req, res) => {
+  res.json("Sign Up Route");
+};
+
+export const login = async (req, res) => {
+  res.json("login Route");
+};
+
+export const logout = async (req, res) => {
+  res.json("logout Route");
+};
--- backend/routes/auth.route.js
+++ backend/routes/auth.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 
+import { login, logout, signup } from "../controllers/auth.controller.js";
+
 const router = Router();
 
-router.get("/signup", (req, res) => {
-  res.json("Sign Up Route");
-});
+router.get("/signup",signup);
+router.get("/login", login);
+router.get("/logout",logout);
 
 export default router;
