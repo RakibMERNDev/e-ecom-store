c121feaa76f6173c2a564cb12a58b1302e9c2999
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-27 21:33:55 +0600

    added hash and compare password on backend

--- backend/models/user.model.js
+++ backend/models/user.model.js
@@ -1,4 +1,5 @@
-import mongoose, { model } from "mongoose";
+import bcrypt from "bcrypt";
+import mongoose from "mongoose";
 
 const userSchema = new mongoose.Schema(
   {
@@ -40,5 +41,21 @@ const userSchema = new mongoose.Schema(
 );
 
 const User = mongoose.model("User", userSchema);
+
+// Pre save hook to save password before saving to Database
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+  } catch (error) {
+    next(error);
+  }
+});
+
+userSchema.methods.comparePassword = async function (password) {
+  return await bcrypt.compare(password, this.password);
+};
 
 export default User;
