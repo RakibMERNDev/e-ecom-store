9db6441bcf13b0369b5dde0982cf8659f8782ea1
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-28 18:40:51 +0600

    added get featured product controller

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,9 +1,11 @@
 import { Router } from "express";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
+import { getAllProducts, getFeaturedProducts } from "../controllers/product.controller.js";
 
 const router = Router();
 
 
 router.get('/',protectRoute, adminRoute, getAllProducts)
+router.get('/featured', getFeaturedProducts)
 
 export default router;
