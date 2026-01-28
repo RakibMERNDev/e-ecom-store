123b2cc63d205e4b2c06cf65824cd18828ac75fa
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-29 05:20:38 +0600

    added coupon route, coupon data model, get coupon controller

--- backend/server.js
+++ backend/server.js
@@ -4,8 +4,9 @@ import express from "express";
 
 import { connectDb } from "./lib/db.js";
 import authRoutes from "./routes/auth.route.js";
-import productRoutes from "./routes/product.route.js";
 import cartRoutes from "./routes/cart.route.js";
+import couponRoutes from "./routes/coupon.route.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
@@ -20,6 +21,7 @@ const PORT = process.env.PORT || 5000;
 app.use("/api/auth", authRoutes);
 app.use("/api/products", productRoutes);
 app.use("/api/cart", cartRoutes);
+app.use("/api/coupons", couponRoutes);
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
