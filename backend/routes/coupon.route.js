3ca896ae06d3cff7269df614af2fae3eeab2d518
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-29 05:26:13 +0600

    added validate coupon controller

--- backend/routes/coupon.route.js
+++ backend/routes/coupon.route.js
@@ -1,8 +1,10 @@
 import { Router } from "express";
 import {protectRoute} from "../middleware/auth.middleware.js";
+import { getCoupon } from "../controllers/coupon.controller.js";
 
 const router = Router();
 
 router.get('/', protectRoute, getCoupon)
+router.post('/validate', protectRoute, validateCoupon)
 
 export default router;
