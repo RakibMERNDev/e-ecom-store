ab3e566521bc7978d3a47fb5cde3f236063222cb
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-28 20:36:21 +0600

    added recommended products controller

--- backend/routes/product.route.js
+++ backend/routes/product.route.js
@@ -1,12 +1,13 @@
 import { Router } from "express";
 import { adminRoute, protectRoute } from "../middleware/auth.middleware.js";
-import { getAllProducts, getFeaturedProducts, createProduct, deleteProduct } from "../controllers/product.controller.js";
+import { getAllProducts, getFeaturedProducts, createProduct, deleteProduct, getRecommendedProducts } from "../controllers/product.controller.js";
 
 const router = Router();
 
 
 router.get('/',protectRoute, adminRoute, getAllProducts)
 router.get('/featured', getFeaturedProducts)
+router.get('/recommendations', getRecommendedProducts)
 router.post('/',protectRoute,adminRoute, createProduct)
 router.delete('/:id',protectRoute,adminRoute, deleteProduct)
 
