58ef1596906c9f71e6a176023c6d6bceaa3c81fe
RakibMernDev <abdullahalrakib30@gmail.com>
2026-01-29 23:03:52 +0600

    added payment controller and stripe session for payment

--- backend/lib/stripe.js
+++ backend/lib/stripe.js
@@ -1,3 +1,8 @@
+import dotenv from "dotenv";
 import stripe, { Stripe } from "stripe";
+
+dotenv.config();
+
+
 
 export const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
--- backend/routes/payment.route.js
+++ backend/routes/payment.route.js
@@ -1,7 +1,10 @@
 import { Router } from "express";
 
+import { protectRoute } from "../middleware/auth.middleware.js";
+import { createCheckoutSession } from "../controllers/payment.controller.js";
+
 const router = Router();
 
-router.post('/create-checkout-session', createCheckoutSession)
+router.post("/create-checkout-session", protectRoute, createCheckoutSession);
 
 export default router;
