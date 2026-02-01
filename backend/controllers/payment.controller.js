6e2655ec430f68137034a95431394b585d9ff39f
RakibMernDev <abdullahalrakib30@gmail.com>
2026-02-01 08:09:25 +0600

    added order model on controller

--- backend/controllers/payment.controller.js
+++ backend/controllers/payment.controller.js
@@ -1,5 +1,6 @@
 import { stripe } from "../lib/stripe.js";
 import Coupon from "../models/coupon.model.js";
+import Order from "../models/order.model.js";
 
 export const createCheckoutSession = async (req, res) => {
   try {
