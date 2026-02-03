8300f0f9e3e0a3697b22c60f7e69d6976f48f90b
RakibMernDev <abdullahalrakib30@gmail.com>
2026-02-04 05:12:47 +0600

    increased upload image limit on backend

--- backend/server.js
+++ backend/server.js
@@ -14,9 +14,9 @@ dotenv.config();
 
 const app = express();
 
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
 
-app.use(cookieParser())
+app.use(cookieParser());
 
 const PORT = process.env.PORT || 5000;
 
@@ -31,4 +31,3 @@ app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
   connectDb();
 });
-
