55517146f043aba344667487de2440e063d53247
RakibMernDev <abdullahalrakib30@gmail.com>
2026-02-06 05:48:10 +0600

    added cart page

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -6,7 +6,7 @@ import { useCartStore } from "../store/useCartStore";
 const Navbar = () => {
   const { user, logout } = useUserStore();
 
-  const {cart} = useCartStore();
+  const { cart } = useCartStore();
 
   const isAdmin = user?.role === "admin";
 
@@ -40,13 +40,14 @@ const Navbar = () => {
                   size={20}
                 />
                 <span className="hidden sm:inline">Cart</span>
-
-                <span
-                  className="absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
+                {cart.length > 0 && (
+                  <span
+                    className="absolute -top-2 -left-2 bg-emerald-500 text-white rounded-full px-2 py-0.5 
 									text-xs group-hover:bg-emerald-400 transition duration-300 ease-in-out"
-                >
-                {cart.length}
-                </span>
+                  >
+                    {cart.length}
+                  </span>
+                )}
               </Link>
             )}
             {isAdmin && (
