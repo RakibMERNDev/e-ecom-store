e69e97b013e7ae18ca5a97a50184e7d99016e390
RakibMernDev <abdullahalrakib30@gmail.com>
2026-02-09 05:39:29 +0600

    added new components

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -4,12 +4,15 @@ import { Link } from "react-router-dom";
 import CartItem from "../components/CartItem";
 import PeopleAlsoBought from "../components/PeopleAlsoBought";
 import { ShoppingCart } from "lucide-react";
+import OrderSummary from "../components/OrderSummary";
+import GiftCouponCard from "../components/GiftCouponCard ";
+
 
 const CartPage = () => {
   const { cart } = useCartStore();
   return (
     <div className="py-8 md:py-16">
-    <div className="mx-auto max-w-screen-7xl px-4 2xl:px-0">
+      <div className="mx-auto max-w-screen-7xl px-4 2xl:px-0">
         <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
           <Motion.div
             className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl"
@@ -28,6 +31,17 @@ const CartPage = () => {
             )}
             {cart.length > 0 && <PeopleAlsoBought />}
           </Motion.div>
+          {cart.length > 0 && (
+            <Motion.div
+              className="mx-auto mt-6 max-w-4xl flex-1 space-y-6 lg:mt-0 lg:w-full"
+              initial={{ opacity: 0, x: 20 }}
+              animate={{ opacity: 1, x: 0 }}
+              transition={{ duration: 0.5, delay: 0.4 }}
+            >
+              <OrderSummary />
+              <GiftCouponCard />
+            </Motion.div>
+          )}
         </div>
       </div>
     </div>
