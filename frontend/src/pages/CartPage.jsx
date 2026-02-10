dad377db1f5f046d96f55945e0419ec8def93ab3
RakibMernDev <abdullahalrakib30@gmail.com>
2026-02-10 06:23:15 +0600

    added order summery

--- frontend/src/pages/CartPage.jsx
+++ frontend/src/pages/CartPage.jsx
@@ -12,7 +12,7 @@ const CartPage = () => {
   const { cart } = useCartStore();
   return (
     <div className="py-8 md:py-16">
-      <div className="mx-auto max-w-screen-7xl px-4 2xl:px-0">
+      <div className="mx-auto max-w-7xl px-4 2xl:px-0">
         <div className="mt-6 sm:mt-8 md:gap-6 lg:flex lg:items-start xl:gap-8">
           <Motion.div
             className="mx-auto w-full flex-none lg:max-w-2xl xl:max-w-4xl"
