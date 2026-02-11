78bfa66c3519f82d2f716b1bb344de1502ec9b7b
RakibMernDev <abdullahalrakib30@gmail.com>
2026-02-11 10:47:34 +0600

    refactor: reorganize import statements in server.js for clarity

--- backend/server.js
+++ backend/server.js
@@ -1,15 +1,15 @@
 import cookieParser from "cookie-parser";
+import cors from "cors";
 import dotenv from "dotenv";
 import express from "express";
-import cors from "cors";
 
 import { connectDb } from "./lib/db.js";
+import analyticsRoutes from "./routes/analytics.route.js";
 import authRoutes from "./routes/auth.route.js";
 import cartRoutes from "./routes/cart.route.js";
 import couponRoutes from "./routes/coupon.route.js";
-import productRoutes from "./routes/product.route.js";
 import paymentRoutes from "./routes/payment.route.js";
-import analyticsRoutes from "./routes/analytics.route.js";
+import productRoutes from "./routes/product.route.js";
 
 dotenv.config();
 
